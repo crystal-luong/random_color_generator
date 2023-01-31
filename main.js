eb8bd6052181dd05a85fb32c19ee520c23439cb2
crystal-luong <balb2k2@gmail.com>
2023-01-31 10:58:45 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const containerEl = document.querySelector(".container");
+
+for(let i = 0; i < 32; ++i) {
+    const colorContainerEl = document.createElement("div");
+    colorContainerEl.classList.add("container-section");
+    containerEl.appendChild(colorContainerEl);
+}
+const colorContainerEls = document.querySelectorAll(".container-section");
+
+function generateColors() {
+     colorContainerEls.forEach(colorContainerEl => { 
+        let newRandomColor = randomColor();
+        colorContainerEl.style.backgroundColor = `#${newRandomColor}`;
+        colorContainerEl.innerHTML = `#${newRandomColor}`;
+     })
+}
+function randomColor() {
+    const chars = "0123456789abcdef";
+    let newColor = [];    
+    for(let i = 0; i < 6; ++i ) {
+        const randomNum = Math.floor(Math.random()*chars.length);
+       
+       const colorCharacter = chars.substring(randomNum,randomNum+1);
+       newColor += colorCharacter;
+      
+    }
+    return newColor;
+}
+generateColors();
+
+
+
+
+
+
+
